b16cc4ff4d87c026b63421757dc3291aaeddf146
Sravan <sravanpabolu@gmail.com>
2022-01-24 17:58:32 +0530

    Comparible

--- iOSPractice.playground/Pages/Comparable.xcplaygroundpage/Contents.swift
+++ iOSPractice.playground/Pages/Comparable.xcplaygroundpage/Contents.swift
@@ -4,4 +4,16 @@ import Foundation
 
 var greeting = "Hello, playground"
 
+struct Person: Comparable {
+    var name: String
+
+    static func <(lhs: Person, rhs: Person) -> Bool {
+        return lhs.name < rhs.name
+    }
+}
+
+let taylor = Person(name: "Taylor Swift")
+let justin = Person(name: "Justin Bieber")
+print(taylor < justin)
+
 //: [Next](@next)
